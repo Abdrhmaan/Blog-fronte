220543820692d3e91d5988994d24ea74f0444089
ARoble <abdulladif0042@gmail.com>
2023-07-22 15:10:42 +0300

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
 import Header from "./Components/Header";
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
-import Home from "./Pages/Home"
+import Home from "./Pages/Home";
 import Blog from "./Pages/Blog";
 import Dashboard from "./Pages/Dashboard";
-import New from "./Pages/New"
-import Profile from "./Pages/Profile"
+import New from "./Pages/New";
+import Profile from "./Pages/Profile";
 import ChangePassword from "./Pages/ChangePassword";
-
 
 function App() {
   return (
-   <div >
+    <div>
       <BrowserRouter>
-      <Header/>
-      <div className="px-20  m-auto">
-        <Routes>
-          <Route exact path="/" element={<Home/>}/>
-          <Route path="/login" element={<Login/>} />
-          <Route path="/signup" element={<Signup/>}/>
-          <Route path="/blog" element={<Blog/>}/>
-          <Route path="/dashboard" element={<Dashboard/>}/>
-          <Route path="/new" element={<New/>}/>
-          <Route path="/profile" element={<Profile/>}/>
-          <Route path="/change" element={<ChangePassword/>}/>
-        </Routes>
+        <Header />
+        <div className="px-32 2xl:px-72 ">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/change" element={<ChangePassword />} />
+          </Routes>
         </div>
       </BrowserRouter>
-
-   </div>
+    </div>
   );
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,27 @@
-import {MdSearch} from "react-icons/md"
-import {Link} from "react-router-dom"
+import { MdSearch } from "react-icons/md";
+import { Link } from "react-router-dom";
 
-function Header(){
-    return (
-        <div className="flex justify-between bg-white py-4 px-20  border-b-2">
-            <div className="flex items-center">
-                <Link to="/">
-                    <img className="h-9" src="https://dev-to-uploads.s3.amazonaws.com/uploads/logos/resized_logo_UQww2soKuUsjaOGNB38o.png"/>
-                </Link>
-                <div className="ml-3 flex items-center border-2 px-2 rounded-md">
-                    <input className="p-1.5 w-72" type="text" placeholder="Search"/>
-                    <MdSearch size={25}/>
-                </div>
-            </div>
-            {/* WHEN USERS NOT LOGGED IN */}
-            {/* <div>
+function Header() {
+  return (
+    <div className="flex justify-between bg-white py-4 px-32 2xl:px-72  border-b-2">
+      <div className="flex items-center">
+        <Link to="/">
+          <img
+            className="h-9"
+            src="https://dev-to-uploads.s3.amazonaws.com/uploads/logos/resized_logo_UQww2soKuUsjaOGNB38o.png"
+          />
+        </Link>
+        <div className="ml-3 flex items-center border-2 px-2 rounded-md">
+          <input
+            className="p-1.5 w-72 focus:border-none"
+            type="text"
+            placeholder="Search"
+          />
+          <MdSearch size={25} />
+        </div>
+      </div>
+      {/* WHEN USERS NOT LOGGED IN */}
+      {/* <div>
                 <Link to="/login">
                 <button className="mr-3">Log in</button>
                 </Link>
@@ -22,22 +29,27 @@ function Header(){
                 <button className="border border-blue-700 py-1.5 px-2.5 rounded-md text-blue-700">Create Account</button>
                 </Link>
             </div> */}
-            {/* WHEN USERS NOT LOGGED IN */}
+      {/* WHEN USERS NOT LOGGED IN */}
 
-            {/* WHEN USER IS LOGGED IN */}
-            <div className="flex items-center">
-                <Link to="/new">
-                <button className="border border-blue-700 py-1.5 px-2.5 rounded-md text-blue-700">Post Blog</button>
-                </Link>
-                <Link to="/dashboard">
-                <div className="h-10 w-10 ml-4">               
-                     <img className="rounded-full" src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"/>
-                </div>
-                </Link>
-            </div>
-            {/* WHEN USER IS LOGGED IN */}
-        </div>
-    )
+      {/* WHEN USER IS LOGGED IN */}
+      <div className="flex items-center">
+        <Link to="/new">
+          <button className="border border-blue-700 py-1.5 px-2.5 rounded-md text-blue-700">
+            Post Blog
+          </button>
+        </Link>
+        <Link to="/dashboard">
+          <div className="h-10 w-10 ml-4">
+            <img
+              className="rounded-full"
+              src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+            />
+          </div>
+        </Link>
+      </div>
+      {/* WHEN USER IS LOGGED IN */}
+    </div>
+  );
 }
 
-export default Header
+export default Header;
